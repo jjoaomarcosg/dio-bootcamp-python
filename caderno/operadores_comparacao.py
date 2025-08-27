83728b2fc0fc69dfaa23554add2749c0f7f0caec
João Marcos <jmsgdd297@gmail.com>
2025-08-26 21:48:51 -0300

    Conclusão aula 7 - Operadores comparativos

--- caderno/operadores_comparacao.py
+++ caderno/operadores_comparacao.py
@@ -1,1 +1,30 @@
 # São operadores utilizados para comparar dois valores
+
+
+# == -> Igualdade
+saldo = 450
+saque = 200
+
+print(saldo == saque)
+
+# != -> Diferença 
+saldo = 450
+saque = 200
+
+print(saldo != saque)
+
+#Maior que / Maior ou igual 
+saldo = 450
+saque = 200
+
+print(saldo > saque)
+print(saldo >= saque)
+
+#Menor que / Menor ou igual 
+saldo = 450
+saque = 200
+
+print(saldo < saque)
+print(saldo <= saque)
+
+
