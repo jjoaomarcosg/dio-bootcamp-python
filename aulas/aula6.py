c73447a58eb7b5af35a65ea9019a87c992a5f166
João Marcos <jmsgdd297@gmail.com>
2025-08-26 21:08:36 -0300

    Concluída aula 6 - Operadores aritmeticos

---
+++ aulas/aula6.py
@@ -0,0 +1,15 @@
+product_1 = 20
+product_2 = 10
+
+print(product_1 + product_2)
+print(product_1 - product_2)
+print(product_1 / product_2)
+print(product_1 // product_2)
+print(product_1 * product_2)
+print(product_1 % product_2)
+print(product_1 ** product_2)
+
+x = (10 + 5) * 4
+y = (10 / 2) + 25 * ((2 - 2) ** 2) 
+print(x)
+print(y)
---
+++ caderno/operadores_aritmeticos.py
@@ -0,0 +1,29 @@
+# # Adição
+# print(1 + 1)
+
+# #Subtração 
+# print(10 - 2)
+
+# # Multiplicação 
+# print(4 * 3)
+
+# #Divisão
+# print(12 / 3)
+
+# #Divisão inteira
+# print(12 //2)
+
+# #Módulo
+# print(10 % 3)
+
+# # Exponenciação
+# print(2 ** 3)
+
+'''
+Ordem de precedencia 
+1 - parentesis
+2 - expoentes 
+3 - multiplicações e divisões (da esquerda pra direita)
+4 - somas e subtrações (da esquerda pra direita)
+
+'''
