0f40205aed4d2c8d44070d83bcd9cd2aa29428fb
João Marcos <jmsgdd297@gmail.com>
2025-08-27 00:32:15 -0300

    Conclusão Aula 11 - Operadores de associação

---
+++ caderno/operadores_associacao.py
@@ -0,0 +1,16 @@
+# OPERADORES DE ASSOCIAÇÃO
+# - São utilizados para verificar se um objeto
+# está presente em uma sequencia
+
+curso = 'Curso de Python'
+frutas = ['laranja', 'uva', 'limão']
+saques = [1500, 100]
+
+'Python' in curso
+#>>> True
+
+'maçã' not in frutas 
+#>>> True
+
+200 in saques
+#>>> False
