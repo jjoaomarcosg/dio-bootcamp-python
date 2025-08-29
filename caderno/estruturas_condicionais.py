4c3acee083c0629503db7464f6b3b65de800cb2b
João Marcos <jmsgdd297@gmail.com>
2025-08-28 23:21:27 -0300

    Conclusão Aula 13 - Estruturas condicionais

---
+++ caderno/estruturas_condicionais.py
@@ -0,0 +1,58 @@
+# AULA 13 - ESTRUTURAS CONDICIONAIS
+# ETAPA 1 - if / is-else / else
+# ETAPA 2 - if aninhado
+# ETAPA 3 - if ternário
+
+'''
+# ESTRUTURAS CONDICIONAIS
+
+- A estrutura condicional permite o desvio de fluxo de controle,
+quando determinadas expressões lógicas são atendidas.
+
+--------------------------- ETAPA 1 ------------------------------
+
+# if 
+
+- Para criar uma estrutura condicional simples, composta por um 
+único desvio, oidemos utilizar if. O comando irá testar a expressão
+logica, e em caso de retorno verdadeiro as ações presentes no bloco
+de codigo do if serão executadas.
+
+# if /else
+
+- Para criar uma estrutura condicional com dois desvios, podemos
+utilizar as palavras reservadas if e else. Como sabemos se a 
+expressão lógica testada no if for verdadeira, então o bloco
+ de código do else será executado.
+
+# in / elif / else 
+
+- Em alguns cenários queremos mais de dois desvios, para isso
+podemos utilizar a palavra reservada elif. O elif é composto por
+uma nova expressão lógica, que será testada e caso retorne 
+verdadeiro o bloco de código do elif será executado. Não existe
+um número máximo de elifs que podemos utilizar, porém evite
+criar grandes estruturas condicionais, pois elas aumentam a 
+complexidade do código.    
+
+
+--------------------------- ETAPA 2 ------------------------------
+
+# IF ANINHADO
+
+- Podemos criar estruturas condicionais aninhadas, para isso 
+basta adicionar estruturas if/elif/else dentro do bloco de 
+código de estruturas if/elif/else.
+
+
+--------------------------- ETAPA 3 ------------------------------
+
+# IF TERNÁRIO
+
+- O if ternário permite escrever uma condição em uma única
+linha. ele é composto por três partes, a primeira parte é o 
+retorno caso a expressão retorne verdadeiro, a segunda parte 
+é a expressão lógica e a terceira parte é o retorno caso a 
+expressão não seja atendida.
+
+'''
