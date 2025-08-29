d8eb997d8028a3a2ad446c714f543aa236d09acc
João Marcos <jmsgdd297@gmail.com>
2025-08-28 21:35:14 -0300

    Conclusão Aula 12 - Identação/ blocos

---
+++ caderno/identacao_blocos.py
@@ -0,0 +1,19 @@
+'''
+# IDENTAÇÃO DE BLOCOS
+
+- Identar código é uma forma de manter o código fonte mais legível e manutenível.
+Mas em Python ela exerce um segundo papel, através da identação o interpretador
+consegue determinar onde um bloco de comando inicia e onde ele termina.
+
+# BLOCO DE COMANDO 
+
+- As linguagens de programação costumam utilizar caracteres ou palavras reservadas 
+para terminar o inicio e fim do bloco. Em Java e C por exemplo, utilizamos caves.
+
+# UTILIZANDO ESPAÇOS
+
+- Existe uma convenção em Python, que define as boas práticas para escrita de código
+na linguagem. Nesse documento e indicado utilizar 4 espaços em branco por nível
+de identação, ou seja, a cada novo bloco adicionamos 4 novos espaços em branco.
+
+'''
